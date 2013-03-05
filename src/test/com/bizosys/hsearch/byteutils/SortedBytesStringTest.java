fe223239769bc581ece1d9bab2ed4c60241cdbed
Shubendu <shubhendu.singh@bizosys.com>
2013-03-05 17:15:51 +0530

    SortedBytesString test for large set
    4 million string list with 12 random strings searched in 3330 mill secs

--- src/test/com/bizosys/hsearch/byteutils/SortedBytesStringTest.java
+++ src/test/com/bizosys/hsearch/byteutils/SortedBytesStringTest.java
@@ -24,7 +24,7 @@ public class SortedBytesStringTest extends TestCase {
 
 		} else if (modes[2].equals(mode)) {
 			t.setUp();
-			t.testAddAll();
+			t.testLargeSet();
 			t.tearDown();
 		}
 
@@ -42,12 +42,6 @@ public class SortedBytesStringTest extends TestCase {
 		List<String> sortedList = new ArrayList<String>();
 		sortedList.add(new String("first"));
 		sortedList.add(new String("test"));
-		sortedList.add(new String("test"));
-		sortedList.add(new String("cos"));
-		sortedList.add(new String("cos"));
-		sortedList.add(new String("sin"));
-		sortedList.add(new String("tan"));
-		sortedList.add(new String(""));
 
 		Collections.sort(sortedList);
 		System.out.println(sortedList);
@@ -59,7 +53,7 @@ public class SortedBytesStringTest extends TestCase {
 		for (int pos : positions) {
 			assertEquals(SortedBytesString.getInstance().parse(bytes).getValueAt(pos), "test");
 		}
-		assertEquals(2, positions.size());
+		assertEquals(1, positions.size());
 	}
 	
 	public void testSingleEqual()throws Exception{
@@ -100,4 +94,44 @@ public class SortedBytesStringTest extends TestCase {
 		
 		assertEquals(sbs.getSize(), 6);
 	}
+	
+	public void testLargeSet() throws Exception {
+		System.out.println("mem size"+Runtime.getRuntime().maxMemory()/(1024 * 1024));
+		List<String> sortedList = new ArrayList<String>();
+		
+		String[] strArr = new String[]{"shubhendu","abinash","sunil","ravi","pramod","udbhav","dhuli","zombie","shubh","shubhend","shu","socks"};
+		int count = 0;
+		for(int i = 0; i < 4000000; i++){
+			int index = (int)(Math.random()*12);
+			sortedList.add(strArr[index]);
+			if(strArr[index].equals("shubhendu"))
+				count++;
+		}
+		
+		System.out.println("No of shubhendu is :"+count);
+		
+		Collections.sort(sortedList);
+
+		byte[] bytes = SortedBytesString.getInstance().toBytes(sortedList);
+		
+		System.out.println("byte length is :"+bytes.length);
+		
+		List<Integer> positions = new ArrayList<Integer>();
+		
+		long start = System.currentTimeMillis();
+		
+		SortedBytesString.getInstance().parse(bytes).getEqualToIndexes("shubhendu", positions);
+
+		assertNotNull(positions);
+		for (int pos : positions) {
+			assertEquals(SortedBytesString.getInstance().parse(bytes).getValueAt(pos), "shubhendu");
+		}
+		
+		long end = System.currentTimeMillis();
+		assertEquals(count, positions.size());
+		
+		System.out.println("Returned shubhendu is "+positions.size() +" in "+(end - start) +" mill secs");
+	}
+
+	
 }
